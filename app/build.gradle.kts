33be265b89af250405b7e5745c74713393eee210
keom-loma <keom.heang@lomatechnology.com>
2025-05-01 16:42:19 +0700

    - Add more firebase app distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.gms.google-services")
+    alias(libs.plugins.google.firebase.appdistribution)
 }
 
 android {
